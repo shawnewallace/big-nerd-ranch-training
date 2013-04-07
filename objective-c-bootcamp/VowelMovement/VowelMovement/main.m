eb85eedb010121170a7db83501323c5900eb7bed
Shawn Wallace <shawn@wshawn.com>
2013-04-07 18:11:31 -0400

    Chapter 32

--- objective-c-bootcamp/VowelMovement/VowelMovement/main.m
+++ objective-c-bootcamp/VowelMovement/VowelMovement/main.m
@@ -10,11 +10,81 @@
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // create the array of strings to devowelize and a container for new ones
+        NSArray *oldStrings = [NSArray arrayWithObjects:
+                               @"Sauerkraut",
+                               @"Raygun",
+                               @"Big Nerd Ranch",
+                               @"Mississippi",
+                               nil];
+        
+        NSLog(@"old strings: %@", oldStrings);
+        NSMutableArray *newStrings = [NSMutableArray array];
+        
+        // create a list of characters that we'll remove from the string
+        NSArray *vowels = [NSArray arrayWithObjects:
+                           @"a",
+                           @"e",
+                           @"i",
+                           @"o",
+                           @"u",
+                           nil];
+        
+        // declare the block variable
+        //        void (^devowelizer)(id, NSUInteger, BOOL *) = ^(id string, NSUInteger i, BOOL *stop) {
+        //
+        //            NSRange yRange = [string rangeOfString:@"y"
+        //                                           options:NSCaseInsensitiveSearch];
+        //            // did I find a y?
+        //            if(yRange.location != NSNotFound) {
+        //                *stop = YES;
+        //                return;
+        //            }
+        //
+        //            NSMutableString *newString = [NSMutableString stringWithString:string];
+        //
+        //            // iterate over the array of vowels, replacing occurrences of each
+        //            // with an empty string
+        //            for (NSString *s in vowels) {
+        //                NSRange fullRange = NSMakeRange(0, [newString length]);
+        //                [newString replaceOccurrencesOfString:s
+        //                                           withString:@""
+        //                                              options:NSCaseInsensitiveSearch
+        //                                                range:fullRange];
+        //            }
+        //
+        //            [newStrings addObject:newString];
+        //        };
+        
+        [oldStrings enumerateObjectsUsingBlock:^(id string, NSUInteger i, BOOL *stop) {
+            
+            NSRange yRange = [string rangeOfString:@"y"
+                                           options:NSCaseInsensitiveSearch];
+            // did I find a y?
+            if(yRange.location != NSNotFound) {
+                *stop = YES;
+                return;
+            }
+            
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            // iterate over the array of vowels, replacing occurrences of each
+            // with an empty string
+            for (NSString *s in vowels) {
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:s
+                                           withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:fullRange];
+            }
+            
+            [newStrings addObject:newString];
+        }
+         ];
+        NSLog(@"new strings: %@", newStrings);
         
     }
     return 0;
